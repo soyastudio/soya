381e5ce927db357396a794e6f7afa8e4bb258aef
wenqun <wenqun.soya@gmail.com>
2019-10-23 07:49:38 -0700

    Project initialize

--- dovetails-core/src/main/java/soya/framework/dovetails/component/context/PropertiesEvaluator.java
+++ dovetails-core/src/main/java/soya/framework/dovetails/component/context/PropertiesEvaluator.java
@@ -2,11 +2,32 @@ package soya.framework.dovetails.component.context;
 
 import java.util.*;
 
-public class PropertyEvaluator {
+public class PropertiesEvaluator {
+    private Properties sources;
+    private Properties values;
 
+    private Map<String, PropertyDescriptor> descriptors = new HashMap<>();
     private Set<PropertyListener> listeners = new HashSet<>();
 
-    public PropertyEvaluator(List<PropertyDescriptor> propertyDescriptors) {
+    public PropertiesEvaluator(Properties sources, Properties values) {
+        this.sources = sources == null ? new Properties() : new Properties(sources);
+        this.values = values == null ? new Properties() : new Properties(values);
+
+        Enumeration<?> enumeration = sources.propertyNames();
+        while (enumeration.hasMoreElements()) {
+            String key = (String) enumeration.nextElement();
+            String value = sources.getProperty(key);
+
+            PropertyDescriptor desc = new PropertyDescriptor(key, value, values);
+            descriptors.put(desc.getName(), desc);
+            if(desc.getParameters().size() > 0) {
+                listeners.add(new PropertyListener(desc));
+            }
+        }
+
+    }
+
+    public PropertiesEvaluator(List<PropertyDescriptor> propertyDescriptors) {
 
 
     }
@@ -37,11 +58,15 @@ public class PropertyEvaluator {
     class PropertyListener implements EventListener {
         private PropertyDescriptor descriptor;
 
+        public PropertyListener(PropertyDescriptor descriptor) {
+            this.descriptor = descriptor;
+        }
+
         public void onEvent(PropertyEvent event) {
             if (descriptor.getParameters().contains(event.getPropertyName())) {
                 List<String> fragments = new ArrayList<>();
-                for(String frag: descriptor.getFragments()) {
-                    if(frag.equals(event.getToken())) {
+                for (String frag : descriptor.getFragments()) {
+                    if (frag.equals(event.getToken())) {
                         fragments.add(event.getPropertyValue());
                     } else {
                         fragments.add(frag);
--- dovetails-core/src/main/java/soya/framework/dovetails/component/context/PropertyDescriptor.java
+++ dovetails-core/src/main/java/soya/framework/dovetails/component/context/PropertyDescriptor.java
@@ -1,9 +1,6 @@
 package soya.framework.dovetails.component.context;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 class PropertyDescriptor {
     private final String name;
@@ -27,6 +24,37 @@ class PropertyDescriptor {
                 fragments.add(b + "}");
 
                 parameters.add(b.substring(2));
+            } else {
+                fragments.add(s);
+            }
+        }
+    }
+
+    public PropertyDescriptor(String name, String value, Properties values) {
+        this.name = name;
+
+        fragments = new ArrayList<>();
+        parameters = new HashSet<>();
+
+        String[] arr = value.split("}");
+        for (String s : arr) {
+            if (s.contains("${")) {
+                int index = s.indexOf("${");
+                String a = s.substring(0, index);
+                String b = s.substring(index);
+
+
+                fragments.add(a);
+                String param = b.substring(2);
+                String v = values.getProperty(param);
+                if(v != null) {
+                    fragments.add(v);
+
+                } else {
+                    fragments.add(b + "}");
+                    parameters.add(param);
+
+                }
             } else {
                 fragments.add(s);
             }

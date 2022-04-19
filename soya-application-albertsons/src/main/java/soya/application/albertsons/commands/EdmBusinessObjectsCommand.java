c8ec79023582c6c1727fbbeb90b0c6602da4422d
Qun Wen <wenqun.soya@gmail.com>
2022-04-18 18:50:10 -0700

    Command Annotation and Codegen

---
+++ soya-application-albertsons/src/main/java/soya/application/albertsons/commands/EdmBusinessObjectsCommand.java
@@ -0,0 +1,83 @@
+package soya.application.albertsons.commands;
+
+import com.google.gson.GsonBuilder;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import soya.framework.core.Command;
+import soya.framework.core.CommandCallable;
+import soya.framework.core.CommandOption;
+
+import java.io.File;
+import java.util.*;
+
+@Command(group = "business-object-edm", name = "edm-bods",
+        httpMethod = Command.HttpMethod.GET, httpResponseTypes = Command.MediaType.APPLICATION_JSON)
+public class EdmBusinessObjectsCommand implements CommandCallable<String> {
+
+    @CommandOption(option = "h", required = true,
+            paramType = CommandOption.ParamType.ReferenceParam, referenceKey = "workspace.home")
+    protected String home;
+
+
+    private String edmMasterMappingFile = "EDM Master Mapping.xlsx";
+    private String indexSheet = "All Tables";
+    private String mappingSheet = "MasterMapping";
+
+    protected File homeDir;
+    protected File edmDir;
+    protected File masterMappingFile;
+
+    @Override
+    public String call() throws Exception {
+        init();
+
+        XSSFWorkbook workbook = new XSSFWorkbook(masterMappingFile);
+        Sheet sheet = workbook.getSheet(mappingSheet);
+
+        Object result = process(sheet);
+
+        workbook.close();
+
+        return render(result);
+    }
+
+    protected void init() {
+        this.homeDir = new File(home);
+        this.edmDir = new File(home, "EDM");
+        this.masterMappingFile = new File(edmDir, edmMasterMappingFile);
+    }
+
+    protected Object process(Sheet sheet) throws Exception {
+        Set<String> set = new LinkedHashSet<>();
+        for (int i = sheet.getFirstRowNum() + 1; i <= sheet.getLastRowNum(); i++) {
+            Row row = sheet.getRow(i);
+            if (row != null && row.getCell(1) != null) {
+                String bod = cellValue(row.getCell(1));
+                if (bod != null && bod.startsWith("Get")) {
+                    set.add(bod.substring(3));
+
+                }
+            }
+        }
+
+        List<String> list = new ArrayList<>(set);
+        Collections.sort(list);
+
+        return list;
+    }
+
+    protected String render(Object result) throws Exception {
+        return new GsonBuilder().setPrettyPrinting().create().toJson(result);
+    }
+
+    protected String cellValue(Cell cell) {
+        try {
+            return cell.getStringCellValue().trim();
+
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
--- soya-application-albertsons/src/main/java/soya/application/albertsons/commands/EdmMasterMappingCommand.java
+++ soya-application-albertsons/src/main/java/soya/application/albertsons/commands/EdmMasterMappingCommand.java
@@ -1,16 +1,13 @@
 package soya.application.albertsons.commands;
 
-import com.google.common.base.CaseFormat;
 import com.google.gson.JsonObject;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import soya.framework.commons.util.CodeBuilder;
 import soya.framework.core.Command;
 
 import java.io.File;
-import java.util.*;
 
 @Command(group = "business-object-edm", name = "edm-master-mapping",
         httpMethod = Command.HttpMethod.GET, httpResponseTypes = Command.MediaType.TEXT_PLAIN)
@@ -29,98 +26,28 @@ public class EdmMasterMappingCommand extends EdmMappingsCommand {
         XSSFWorkbook workbook = new XSSFWorkbook(file);
         Sheet sheet = workbook.getSheet(mappingSheet);
 
-        if(businessObject.equals("*") ) {
-            for (int i = sheet.getFirstRowNum() + 1; i <= sheet.getLastRowNum(); i++) {
-                Row row = sheet.getRow(i);
-                if (row != null && row.getCell(1) != null) {
-                    String bod = cellValue(row.getCell(1));
-                    if (bod != null && bod.startsWith("Get")) {
-                        EdmDataUnit unit = rowData(row, propName, columnIndex);
+        String boName = "Get" + businessObject;
+        for (int i = sheet.getFirstRowNum() + 1; i <= sheet.getLastRowNum(); i++) {
+            Row row = sheet.getRow(i);
+            if (row != null && row.getCell(1) != null) {
+                String bod = cellValue(row.getCell(1));
+                if (bod != null && bod.equals(boName)) {
+                    EdmDataUnit unit = rowData(row, propName, columnIndex);
 
-                        EdmTable table = tables.get(unit.getTableName().toUpperCase());
-                        if (table == null) {
-                            table = new EdmTable();
-                            table.name = unit.getTableName();
-                            tables.put(table.name.toUpperCase(), table);
-                        }
+                    EdmTable table = tables.get(unit.getTableName().toUpperCase());
+                    if (table == null) {
+                        table = new EdmTable();
+                        table.name = unit.getTableName();
+                        tables.put(table.name.toUpperCase(), table);
                     }
-                }
-            }
 
-        } else {
-            String boName = "Get" + businessObject;
-            for (int i = sheet.getFirstRowNum() + 1; i <= sheet.getLastRowNum(); i++) {
-                Row row = sheet.getRow(i);
-                if (row != null && row.getCell(1) != null) {
-                    String bod = cellValue(row.getCell(1));
-                    if (bod != null && bod.equals(boName)) {
-                        EdmDataUnit unit = rowData(row, propName, columnIndex);
+                    table.columns.put(unit.columnName.toUpperCase(), unit);
 
-                        EdmTable table = tables.get(unit.getTableName().toUpperCase());
-                        if (table == null) {
-                            table = new EdmTable();
-                            table.name = unit.getTableName();
-                            tables.put(table.name.toUpperCase(), table);
-                        }
-
-                        table.columns.put(unit.columnName.toUpperCase(), unit);
-
-                    }
                 }
             }
         }
 
         workbook.close();
-    }
-
-    protected void annotate() throws Exception {
-
-        String mainTable = CaseFormat.UPPER_CAMEL.to(CaseFormat.UPPER_UNDERSCORE, businessObject);
-        String mainTablePK = mainTable + "_ID";
-
-        if(tables.containsKey(mainTable)) {
-            EdmTable table = tables.get(mainTable);
-            table.entityType = "ROOT";
-
-            String pk = table.getName() + "_Id";
-            table.getColumns().forEach(e -> {
-                String columnName = e.getColumnName();
-                if(e.getColumnName().endsWith("_Id")) {
-                    if(columnName.equalsIgnoreCase(pk)) {
-                        System.out.println("------------ pk: " + columnName);
-
-                    } else {
-                        String token = columnName.substring(0, columnName.length() - 3).toUpperCase();
-                        if(tables.containsKey(token)) {
-                            System.out.println("------------ fk: " + columnName + " referTo " + tables.get(token).getName());
-                        }
-                    }
-
-                }
-            });
-
-            if(table.columns.containsKey(pk)) {
-                System.out.println("===================== " + table.columns.get(pk));
-            }
-        }
-
-        tables.values().forEach(e -> {
-            System.out.print("------ " + e.getName() + ": ");
-            if(e.getName().equalsIgnoreCase(mainTable)) {
-                System.out.println("ROOT");
-                e.entityType = "ROOT";
-
-            } else if(e.columns.containsKey(mainTablePK)) {
-                System.out.println("DEPENDENT");
-                e.entityType = "DEPENDENT";
-
-            } else {
-                System.out.println("REFERENCE");
-                e.entityType = "REFERENCE";
-
-            }
-        });
-
     }
 
     private EdmDataUnit rowData(Row row, String[] propName, int[] columnIndex) {
